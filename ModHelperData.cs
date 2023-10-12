ee3925cf7d4687242f9071f5f95a719151bae96b
SashaLizard <141981751+SashaLizard@users.noreply.github.com>
2023-10-11 23:58:57 -0300

    Fixed README and ModHelperData

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,12 +2,12 @@ namespace SashaTest;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "37.3";
+    public const string WorksOnVersion = "39.0";
     public const string Version = "1.0.0";
     public const string Name = "SashaTest";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Learning to mod.";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "SashaLizard"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "SashaTest"; // TODO add your repo name here, also in the download url in README.md
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# SashaTest
- 
+<a href="https://github.com/sashalizard/sashatest/releases/latest/download/SashaTest.dll">
+<img align="left" alt="Icon" height="90" src="Icon.png">
+<img align="right" alt="Download" height="75" src="https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/BloonsTD6%20Mod%20Helper/Resources/DownloadBtn.png">
+
+
+<h1 align="center">Sasha Test</h1>
+
+Learning to mod.
+
+![Screenshot of Time Machine UI](Screenshot.png)
+
+[![Requires BTD6 Mod Helper](https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/banner.png)](https://github.com/gurrenm3/BTD-Mod-Helper#readme)
